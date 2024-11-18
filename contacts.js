d9aeff5120031e3432530c1b3bbf072a2a1d4d68
Corina Ionelia <corynaionelia@gmail.com>
2024-11-18 19:20:56 +0200

    2
    2

--- contacts.js
+++ contacts.js
@@ -2,3 +2,19 @@ const fs = require('fs').promises;
 const path = require('path');
 
 const contactsPath = path.join(__dirname, 'db', 'contacts.json');
+
+
+// Definirea funcției listContacts
+async function listContacts() {
+    try {
+        const data = await fs.readFile(contactsPath, 'utf-8');
+        return JSON.parse(data);
+    } catch (error) {
+        console.error('Eroare la citirea contactelor:', error.message);
+    }
+}
+
+// Exportarea funcției listContacts
+module.exports = {
+    listContacts,
+};
---
+++ index.js
@@ -0,0 +1,27 @@
+const { listContacts, getContactById, removeContact, addContact } = require('./contacts');
+const argv = require('yargs').argv;
+
+async function invokeAction({ action, id, name, email, phone }) {
+    switch (action) {
+        case 'list':
+            console.table(await listContacts());
+            break;
+
+        case 'get':
+            console.log(await getContactById(id));
+            break;
+
+        case 'add':
+            console.log(await addContact(name, email, phone));
+            break;
+
+        case 'remove':
+            console.log(await removeContact(id));
+            break;
+
+        default:
+            console.warn('Unknown action type!');
+    }
+}
+
+invokeAction(argv);
